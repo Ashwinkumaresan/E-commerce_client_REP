36cfdd72126dee9420f2bbe47e9788ece85fe0b7
Ashwinkumaresan <achusuchu123@gmail.com>
2025-10-07 21:03:23 +0530

    Main home

--- src/pages/desktop/Home/Home.jsx
+++ src/pages/desktop/Home/Home.jsx
@@ -96,7 +96,6 @@ export const Home = () => {
             </div>
         )
 
-
     const getToken = () => {
         const tokenOfDealerAdmin = localStorage.getItem("accessTokenDealer")
         if (tokenOfDealerAdmin) {
